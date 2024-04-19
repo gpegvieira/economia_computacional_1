8bcea29ef46e1d4a1253a7a5ad23d6d3aa03b66d
gpegvieira <166852323+gpegvieira@users.noreply.github.com>
2024-04-19 17:04:54 -0300

    Add files via upload

---
+++ soma/soma001.py
@@ -0,0 +1,37 @@
+"""
+Programa soma
+Descrição: Este é um modelo de programa escrito no paradigma sequencial no qual se calcula a soma de dois números.
+Versão: 0.0.1
+Data: 03/04/2024
+"""
+
+
+
+# Alocação de memória
+
+numero_1 = 7
+
+numero_2 = 3
+
+soma = 0
+
+# Entrada de dados
+
+
+
+
+
+
+# Processamento de dados
+
+
+soma = numero_1 + numero_2
+
+
+
+# Saída de dados
+
+print(soma)
+
+
+
---
+++ soma/soma002.py
@@ -0,0 +1,30 @@
+"""
+Programa soma
+Descrição: Este é um modelo de programa escrito no paradigma sequencial no qual se calcula a soma de dois números lidos do teclado.
+Versão: 0.0.2
+Data: 03/04/2024
+"""
+
+
+
+# Alocação de memória
+
+numero_1: int = 0
+
+numero_2: int = 0
+
+soma: int = 0
+
+# Entrada de dados
+
+numero_1 = input("Digite a primeira parcela: ")
+
+numero_2 = input("Digite a segunda parcela: ")
+
+# Processamento de dados
+
+soma = numero_1 + numero_2
+
+# Saída de dados
+
+print(soma)
