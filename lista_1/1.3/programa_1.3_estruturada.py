c79497f4f49ff9d86fe9715671fbe940ba181d92
gpegvieira <166852323+gpegvieira@users.noreply.github.com>
2024-04-19 22:04:52 -0300

    Update programa_1.3_002.py

--- lista_1/1.3/programa_1.3_estruturada.py
+++ lista_1/1.3/programa_1.3_estruturada.py
@@ -1,4 +1,12 @@
-# 1.3 (estruturada)
+"""
+Programa 1.3
+Descrição: Este programa lê dois números do teclado, calcula a sua soma e imprime na tela o resultado.
+Autor: GPV
+Versão: 0.0.2
+Correção realizada: diferentemente da versão 0.0.1, que utiliza linguagem sequencial, a presente versão utiliza linguagem estruturada.
+Data: 12/04/2024
+"""
+
 i = 0
 numero = 0
 soma = 0
