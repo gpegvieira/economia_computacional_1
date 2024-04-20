3f582f1cc9df41c6982ac757cdf7308e2d087ece
gpegvieira <166852323+gpegvieira@users.noreply.github.com>
2024-04-19 22:12:06 -0300

    Update programa_1.4.py

--- lista_1/1.4/programa_1.4.py
+++ lista_1/1.4/programa_1.4.py
@@ -1,3 +1,10 @@
-# 1.4
+"""
+Programa 1.4
+Descrição: Este programa pergunta o nome do usuário e imprime uma saudação, seguida do nome.
+Autor: GPV
+Data: 12/04/2024
+Versão: 0.0.1
+"""
+
 nome = input("Qual é o seu nome?")
 print(f"Oi, {nome}!")
