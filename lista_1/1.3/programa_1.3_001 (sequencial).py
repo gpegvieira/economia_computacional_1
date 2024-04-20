2aba2e1789684c815867b2f87109d4c0b8a7771a
gpegvieira <166852323+gpegvieira@users.noreply.github.com>
2024-04-19 22:11:13 -0300

    Create programa_1.3_001 (sequencial).py

---
+++ lista_1/1.3/programa_1.3_001 (sequencial).py
@@ -0,0 +1,12 @@
+"""
+Programa 1.3
+Descrição: Este programa lê dois números do teclado, calcula a sua soma e imprime na tela o resultado. Esta versão utiliza a linguagem sequencial.
+Autor: GPV
+Versão: 0.0.1
+Data: 12/04/2024
+"""
+
+a = float(input("Digite o primeiro número:"))
+b = float(input("Digite o segundo número:"))
+soma = a + b
+print(soma)
