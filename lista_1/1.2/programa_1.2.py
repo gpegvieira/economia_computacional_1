4d0da6731e7e59bf27c0d81a80cc6c227987a47d
gpegvieira <166852323+gpegvieira@users.noreply.github.com>
2024-04-19 22:02:32 -0300

    Update programa_1.2.py

--- lista_1/1.2/programa_1.2.py
+++ lista_1/1.2/programa_1.2.py
@@ -1,4 +1,11 @@
-# 1.2
+"""
+Programa 1.2
+Descrição: Este programa armazena os números 2 e 3, calcula a sua soma e imprime na tela o resultado.
+Autor: GPV
+Versão: 0.0.1
+Data: 12/04/2024
+"""
+
 a = 2
 b = 3
 soma = a + b
